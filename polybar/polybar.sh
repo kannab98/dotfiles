377a1a378376fb9c8f26881437cf9e67fa2a9b94
Kirill Ponur <ponur0kirill@gmail.com>
2020-03-15 12:04:21 +0300

    add few scripts

---
+++ polybar/polybar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Завершить текущие экземпляры polybar
+killall -q polybar
+
+# Ожидание полного завершения работы процессов
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Запуск Polybar со стандартным расположением конфигурационного файла в ~/.config/polybar/config
+polybar -c $polybar/polybar.conf bottom &
+polybar -c $polybar/polybar.conf top &
